e4f89c92cb2d6da0e07078f7a729f956d7fcc364
Omar EL BARAKA <elbarakaomar99@hotmail.com>
2025-05-27 12:22:39 +0200

    init Backend

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+// index.js
+
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const app = express();
+const prisma = new PrismaClient();
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
