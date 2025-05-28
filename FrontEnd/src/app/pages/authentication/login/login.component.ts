af787d2823e091920517d688d40422c2dd5d4501
Ark1ight <benoi1.suy@gmail.com>
2025-05-28 11:47:33 +0200

    basic front for login created

--- FrontEnd/src/app/pages/authentication/login/login.component.ts
+++ FrontEnd/src/app/pages/authentication/login/login.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: [''],
+      password: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
