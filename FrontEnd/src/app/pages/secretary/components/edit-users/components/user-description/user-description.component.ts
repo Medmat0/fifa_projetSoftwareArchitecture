8538d92c3a13b5dc4f2a6ce0c07d73aba92d4593
Ark1ight <benoi1.suy@gmail.com>
2025-05-30 21:07:52 +0200

    implemented tests for user creation component also for user modification

--- FrontEnd/src/app/pages/secretary/components/edit-users/components/user-description/user-description.component.ts
+++ FrontEnd/src/app/pages/secretary/components/edit-users/components/user-description/user-description.component.ts
@@ -1,9 +1,12 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {User} from '../../../../../../shared/models/user';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-user-description',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './user-description.component.html',
   standalone: true,
   styleUrl: './user-description.component.scss'
