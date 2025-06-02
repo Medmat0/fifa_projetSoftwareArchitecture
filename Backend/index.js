04ca6715c82f8209cf08c4b7f2c4a6bdfe54f5a5
Omar EL BARAKA <elbarakaomar99@hotmail.com>
2025-06-02 08:03:08 +0200

    check-in frontend

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:4300',
+  origin: 'http://localhost:4200',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
