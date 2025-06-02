8d14cecca8ba047e6544728a398d411b206afed9
Omar EL BARAKA <elbarakaomar99@hotmail.com>
2025-06-02 08:03:59 +0200

    Fix index

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: 'http://localhost:4300',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
